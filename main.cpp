5886c065e7208fafdffb21f497e67cd26046cb30
Steven Arizandieta <3817207210101@ingenieria.usac.edu.gt>
2025-12-06 14:54:52 -0600

    [ADD] Listas simples enlazadas y listas dobles
    - Se añadió la estructura inicial de las listas por usar

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include "ListaSimple.h"
+#include "ListaDoble.h"
+
+int main() {
+    /*ListaSimple miLista;
+    miLista.insertarInicio(10);
+    miLista.insertarFinal(15);
+    miLista.insertarInicio(5);
+    miLista.insertarFinal(20);
+
+    miLista.visualizarLista();*/
+
+    cout << endl;
+
+    /*ListaDoble miListaDoble;
+    miListaDoble.insertarInicio(200);
+    miListaDoble.insertarInicio(150);
+    miListaDoble.insertarInicio(100);
+    miListaDoble.insertarInicio(50);
+    miListaDoble.insertarInicio(250);
+
+    miListaDoble.visualizarLista();*/
+    return 0;
+}
